5dfc90553fa5442894269b95dc22803c5adc44a0
Eric <ekellogg90@gmail.com>
2024-06-25 13:49:25 -0500

    code for category model

--- db/schema.sql
+++ db/schema.sql
@@ -3,4 +3,23 @@ DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
+\c ecommerce_db;
+
+CREATE TABLE category (
+
+);
+
+CREATE TABLE product (
+
+);
+
+CREATE TABLE tag (
+
+);
+
+CREATE TABLE productTag (
+
+);
+
+
 
