5f79c6bf2c1c28c4b0f1ff7a15a2fa595396e28f
Eric <ekellogg90@gmail.com>
2024-06-25 18:19:13 -0500

    added through routes for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // find all products
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category }, { model: Tag }],
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'prod_tag'}],
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -22,7 +22,7 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: Tag }],
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'prod_tag' }],
     });
     if (!productData) {
       res.status(404).json({ message: 'No Product found at this ID' });
@@ -36,7 +36,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', /*async*/ (req, res) => {
+router.post('/', (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -45,11 +45,6 @@ router.post('/', /*async*/ (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-//  try {
-//   const productData = await Product.create(req.body);
-//  } catch (err) {
-//   res.status(500).json(err);
-//  }
 
   Product.create(req.body)
     .then((product) => {
